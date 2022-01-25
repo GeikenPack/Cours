513421b926a3ca163f3857ee4daed88d5fe49faa
GeikenPack <jomijo6936@gmail.com>
2022-01-25 17:51:38 +0100

    Ajout du 25/01

--- S4/CDAM/TP1/EnvoieSMS/app/src/main/java/com/example/envoiesms/MainActivity.java
+++ S4/CDAM/TP1/EnvoieSMS/app/src/main/java/com/example/envoiesms/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.envoiesms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && this.checkSelfPermission(android.Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED )
+        {
+            requestPermissions( new String[]{android.Manifest.permission.SEND_SMS,}, 1 );
+        }
+    }
+
+    public void sendSMS(View view)
+    {
+        Context context = getApplicationContext();
+        EditText numeroChamp = (EditText) findViewById(R.id.send_numero);
+        String numero = numeroChamp.getText().toString();
+
+        EditText messageChamp = (EditText) findViewById(R.id.send_message);
+        String message = messageChamp.getText().toString();
+
+        if (numero.length() >= 4 && message.length() > 0) {
+            SmsManager.getDefault().sendTextMessage(numero, null, message, null, null);
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, "Message envoyé", duration);
+            toast.show();
+        }
     }
 }
---
+++ S4/CII/TP1/test.cbl
@@ -0,0 +1,1 @@
+       77 var1 PIC 9(5) value=0.
